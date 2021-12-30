eaabbbd19ce8a0110f3ae37fa58baebb59ef7e18
JesseCodes <88518880+Jesse3421@users.noreply.github.com>
2021-12-30 14:43:41 -0600

    cleaning up routes and controller

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+
+const {
+    getAllThought,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+
+//api/thoughts
+router
+    .route('/')
+    .get(getAllThought)
+    .post(createThought);
+
+//api/thoughts/:id
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .create(createThought)
+    .put(updateThought)
+    .delete(deleteThought)
+
+//api/thoughts/:thoughtId/reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction)
+    .delete(deleteReaction)
+
+module.exports = router;
